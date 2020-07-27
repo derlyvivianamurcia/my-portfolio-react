ebb7717827059e78ca98f95e60e088ec665b8391
Derly <derlyvivianamurcia@gmail.com>
2020-07-27 12:09:47 -0500

    Actualizacion

--- src/components/secAcerca.jsx
+++ src/components/secAcerca.jsx
@@ -9,11 +9,9 @@ const secAcerca = () => {
                 <div className="col-lg-8 text-center">
                     <div className="main_title">
                         <h2 className='title text-fluid'>SOBRE MI</h2>
-                        <p className='perfil'>
-                            <strong>  Ingeniera de sistemas </strong> con conocimiento y habilidad para planear, analizar, diseñar, implementar, medir y verificar sistemas de información. Además, con capacidad Investigativa para identificar determinadas
-                características y poder analizar los posibles problemas en un contexto determinado, y de esta manera proponer y generar soluciones. Me caracterizo por ser una persona responsable, honesta, dinámica con buenas relaciones interpersonales,
-                con profesionalismo y objetividad, para lograr las metas de productividad trazadas por la empresa. </p>
-                        <p><strong>Actualmente:</strong> Me encuentro en formación Front-End con enfoque React, en la Academia GEEK. </p>
+                        <p className='perfil'>                     
+                            <strong>  Ingeniera de sistemas, Desarrolladora Web Front-End con énfasis en React.js, HTML, CSS y JavaScript;  </strong> con habilidades para planear, diseñar e implementar sistemas de información. Soy una profesional proactiva, con buenas relaciones interpersonales y alta motivación por aprender.  </p>
+                       
                         <img className='img-fluid' src={ImagenPersonal} ></img>
                     </div>
                 </div>
